c8d0062caa15e3a8fbc30bcff5398c4f2da23d6f
Tim James <32233010+tjames222@users.noreply.github.com>
2020-09-11 20:26:42 -0700

    Registration page design

---
+++ blog/src/data/PostDataAccessInterface.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+@Local
+public interface PostDataAccessInterface<T> extends DataAccessInterface<T> {
+	public T get(String id);
+	List<T> getAll();
+	void save(T t);
+	void update(String id, T t);
+	void delete(T t);
+	void deletePost(String id, T t);
+}
